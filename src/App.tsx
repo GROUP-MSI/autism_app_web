8894fd220f3badeee6db6cef02f40fe470e4b922
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2025-05-17 23:10:03 -0400

    feat: Demo Admin Route Private

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { HomeRouter } from './routes/HomeRouter'
 import { RedirectorHome } from './components/redirector/RedirectorHome'
+import { PrivateAdminRoute } from './components/redirector/PrivateRoute'
+import { AdminRouter } from './routes/AdminRouter'
 
 function App() {
 
@@ -11,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/home/*" element={ <HomeRouter /> } />
         {/* <Route path="/customer/*" element={ <PrivateCustomerRoute element={<CustomerRouter/>} /> } />
-        <Route path="/worker/*" element={ <PrivateWorkerRoute element={<WorkerRouter/>} /> } />
-        <Route path="/admin/*" element={ <PrivateAdminRoute element={<AdminRouter/>} /> } /> */}
+        <Route path="/worker/*" element={ <PrivateWorkerRoute element={<WorkerRouter/>} /> } /> */}
+        <Route path="/admin/*" element={ <PrivateAdminRoute element={<AdminRouter/>} /> } />
         <Route path="*" element={ <RedirectorHome /> } />
       </Routes>
       <ToastContainer autoClose={1300} className="custom-toast-container" />
---
+++ src/routes/AdminRouter.tsx
@@ -0,0 +1,42 @@
+// import { Route, Routes} from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { UsersDel } from "../views/Users/UserDel";
+
+// import { Users, Movements, Products, Shelves, StreamArm } from '../views';
+// import { Layout } from '../components';
+
+export const AdminRouter = () => {
+  // const [appSections, setAppSections] = useState<AppSectionItem[]>([]);
+  
+  // useRefreshToken();
+
+  // useEffect(() => {
+  //   const fetchSections = async () => {
+  //     const sections = await useAppSections();
+  //     setAppSections(sections);
+  //   };
+
+  //   fetchSections();
+  // }, []);
+
+
+  return (
+    <>
+    <div>
+      {/* <SliderBar items={appSections}>
+        <Routes>
+          <Route path="/*"  element={ <RedirectorAdmin /> } />
+          <Route path='/admins' element={ <Admins /> } />
+          <Route path='/areas' element={ <Areas /> } />
+          <Route path='/workers' element={ <Workers /> } />
+          <Route path='/companies' element={ <PageCompanies /> } />
+          <Route path='/customers' element={ <CustomersAdmin /> } />
+          <Route path='/partners' element={ <PartnersAdmin /> } />
+          <Route path='/roles' element={ <Roles /> } />
+          <Route path='/permissions' element={ <Permissions /> } />
+        </Routes>
+      </SliderBar>   */}
+    </div>
+    </>
+  )
+}
